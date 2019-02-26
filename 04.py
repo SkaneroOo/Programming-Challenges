e1b0a32da7e451ae49ff47a7da820da4b6261f24
SkaneroOo <40175161+SkaneroOo@users.noreply.github.com>
2019-02-26 14:06:38 +0100

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,22 +1,24 @@
+from random import randint
 szyfr=['a','ą','b','c','ć','d','e','ę','f','g','h','i','j','k','l','ł','m','n','ń','o','ó','p','q','r','s','ś','t','u','v','w','x','y','z','ż','ź']
 print('1. Zaszyfruj')
 print('2. Odszyfruj')
 z=int(input())
 e=''
+rand=randint(1,35)
 if z==1:
     b=input('Wprowadź wyraz do zaszyfrowania ')
     b=b.lower()
     c=list(b)
     for d in range(0,len(c)):
         temp=szyfr.index(c[d])
-        if temp==34:
+        if temp>=34-rand:
             temp-=35
-        e=e+szyfr[temp+1]
+        e=e+szyfr[temp+rand]
 elif z==2:
     b=input('Wprowadź wyraz do odszyfrowania ')
     b=b.lower()
     c=list(b)
-    for d in range(0,len(c)):
+    for d in range(1,len(c)):
         temp=szyfr.index(c[d])
-        e=e+szyfr[temp-1]
-print(e)
+        e=e+szyfr[temp-int(c[0])]
+print(str(e))
