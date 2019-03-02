e502f0bc5ef5baa48198a4519cfd51e9b570b105
SkaneroOo <40175161+SkaneroOo@users.noreply.github.com>
2019-03-02 19:25:28 +0100

    Update odpowiedzi
    losowe odpowiedzi na powitanie

--- run.py
+++ run.py
@@ -2,24 +2,29 @@ from math import *
 from random import randint
 import urllib.request
 try:
-    hi=[line.strip() for line in open("Data\hi.txt", 'r')]
-    calc=[line.strip() for line in open("Data\calc.txt", 'r')]
-#    joke=[line.strip() for line in open("Data\joke.txt", 'r')]
-#    jokes=[line.strip() for line in open("Data\jokes.txt", 'r')]
+    qhi=[line.strip() for line in open("Data/Questions/hi.txt", 'r')]
+    calc=[line.strip() for line in open("Data/Questions/calc.txt", 'r')]
+    ahi=[line.strip() for line in open("Data/Answers/hi.txt", 'r')]
 except FileNotFoundError:
     print('Przygotowywanie wstępnej konfiguracji...')
     url1='https://pastebin.com/raw/pxfnYRTr'
     url2='https://pastebin.com/raw/6C8KKdkB'
-    urllib.request.urlretrieve(url1, 'Data/calc.txt')
-    urllib.request.urlretrieve(url2, 'Data/hi.txt')
+    url3='https://pastebin.com/raw/qtVKPm0F'
+    urllib.request.urlretrieve(url1, 'Data/Questions/calc.txt')
+    urllib.request.urlretrieve(url2, 'Data/Questions/hi.txt')
+    urllib.request.urlretrieve(url3, 'Data/Answers/hi.txt')
+    print('Konfiguracja przebiegła pomyślnie.')
+    qhi=[line.strip() for line in open("Data/Questions/hi.txt", 'r')]
+    calc=[line.strip() for line in open("Data/Questions/calc.txt", 'r')]
+    ahi=[line.strip() for line in open("Data/Answers/hi.txt", 'r')]
 
 while True:
     ui=input(">>> ")
     ui=ui.lower()
-    if ui in hi:
-        print ('Witaj')
+    if ui in qhi:
+        print (ahi[randint(0,len(ahi)-1)])
     elif ui in calc:
-        calcin=input('Co mam policzyć >>> ')
-        print(eval(calcin))
+        arg=input('Co mam policzyć >>> ')
+        print(eval(arg))
     else:
         print ('Przepraszam, nie rozumiem')
